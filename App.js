a6a12721fad3620cc274347d38d36bde846008ce
khlee <rkdgusdl92@gmail.com>
2017-11-28 23:23:10 +0900

    [CHANGED] Data Management

--- App.js
+++ App.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  View,
-} from 'react-native';
+import { View } from 'react-native';
 
 import Main from './src/Container/main';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Main
-          size={80}
-          color={'darkblue'}
-        />
+      <View>
+        <Main />
       </View>
     );
   }
--- src/Container/main.js
+++ src/Container/main.js
@@ -1,46 +1,51 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  StyleSheet,
-  View,
-} from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    fontSize: 12,
-  },
-});
+import List from '../Component/List'
+import Input from '../Component/Input'
+import Title from '../Component/Title'
 
 class Main extends Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      count: 0,
-    }
+      todos: ['Click to remove', 'Learn React Native', 'Write Code', 'Ship App'],
+    };
   }
   
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        count: this.state.count + 1,
-      })
-    }, 1000)
-  }
+  
+  onAddTodo = (text) => {
+    const {todos} = this.state;
+    
+    this.setState({
+      todos: [text, ...todos],
+    })
+  };
+  
+  onRemoveTodo = (index) => {
+    const {todos} = this.state;
+    
+    this.setState({
+      todos: todos.filter((todo, i) => i !== index),
+    })
+  };
   
   render() {
-    const {
-      color,
-      size,
-    } = this.props;
-    const {
-      count,
-    } = this.state;
+    const {todos} = this.state;
     return (
       <View>
-        <Text style={{color, fontSize: size}}>
-          {count}
-        </Text>
+        <Title>
+          To-Do List
+        </Title>
+        <Input
+          placeholder={'Type a todo, then hit enter!'}
+          onSubmitEditing={this.onAddTodo}
+        />
+        <List
+          list={todos}
+          onPressItem={this.onRemoveTodo}
+        />
       </View>
     )
   }
